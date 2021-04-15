dd4eec3fd96d61492b2c6a6c2703fb2b5d452ac4
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-04-15 02:48:41 +0100

    changed totalProjectItems to dynamic content

--- components/shared/tables/TableProjectItems.jsx
+++ components/shared/tables/TableProjectItems.jsx
@@ -1,100 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import DropdownAction from '~/components/elements/basic/DropdownAction';
+import { getProducts } from '~/store/products/action';
 
 const TableProjectItems = () => {
-    const productItems = [
-        {
-            name: 'Herschel Leather Duffle Bag In Brown Color',
-            sku: 'AB123456789-1',
-            stock: 'true',
-            price: '£125.30',
-            date: '2019/11/06',
-            categories: [
-                {
-                    name: 'Bags',
-                },
-                {
-                    name: 'Clothing & Apparel',
-                },
-            ],
-        },
-        {
-            name: 'Apple iPhone Retina 6s Plus 64GB',
-            sku: 'CD987654316-1',
-            stock: 'true',
-            price: '£1,249.99',
-            date: '2018/12/11',
-            categories: [
-                {
-                    name: 'Computers & Technologies',
-                },
-                {
-                    name: 'Technologies',
-                },
-            ],
-        },
-        {
-            name: 'Marshall Kilburn Portable Wireless Speaker',
-            sku: 'SF1133569600-1',
-            stock: 'true',
-            price: '£36.78',
-            date: '2018/12/11',
-            categories: [
-                {
-                    name: 'Babies & Moms',
-                },
-                {
-                    name: 'Refrigerators',
-                },
-            ],
-        },
-        {
-            name: 'Xbox One Wireless Controller Black Color',
-            sku: 'AB123456788',
-            stock: 'false',
-            price: '£55.30',
-            date: '2018/12/11',
-            categories: [
-                {
-                    name: 'Accessories',
-                },
-                {
-                    name: 'Air Conditioners',
-                },
-            ],
-        },
-        {
-            name: 'Grand Slam Indoor Of Show Jumping Novel',
-            sku: 'AB1234567899',
-            stock: 'false',
-            price: '£32.39',
-            date: '2018/12/11',
-            categories: [
-                {
-                    name: 'Books & Office',
-                },
-                {
-                    name: 'Cars & Motocycles',
-                },
-            ],
-        },
-        {
-            name: 'Rayban Rounded Sunglass Brown Color',
-            sku: 'AB123456783',
-            stock: 'true',
-            price: '£321.39',
-            date: '2018/12/11',
-            categories: [
-                {
-                    name: 'Clothing & Apparel',
-                },
-                {
-                    name: 'Cars & Motocycles',
-                },
-            ],
-        },
-    ];
-    const tableItems = productItems.map((item, index) => {
+
+    const dispatch = useDispatch();
+    const fetchProducts = () => {
+        dispatch(getProducts());
+    }
+
+    const state = useSelector(state => ({
+        ...state.allProducts
+    }))
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+
+    const tableItems = allProducts?.map((item, index) => {
         let badgeView;
         if (item.stock) {
             badgeView = <span className="ps-badge success">Stock</span>;
