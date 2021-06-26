888658d233e1c0781e2ead022c66fe3babdb913c
AyariAhmed <ayari17@hotmail.fr>
2021-06-26 22:23:20 +0100

    loggout implemented

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,12 @@ import React, {useEffect, useState} from 'react';
 import CardRecentOrders from '~/components/shared/cards/CardRecentOrders';
 import CardSaleReport from '~/components/shared/cards/CardSaleReport';
 import CardEarning from '~/components/shared/cards/CardEarning';
-// import CardStatics from '~/components/shared/cards/CardStatics';
 import ContainerDashboard from '~/components/layouts/ContainerDashboard';
 import { useDispatch } from 'react-redux';
 import { toggleDrawerMenu } from '~/store/app/action';
 import { connect } from 'react-redux';
 import { useRouter } from 'next/router'
 
-// import CardTopCountries from '~/components/shared/cards/CardTopCountries';
 
 const Index = (props) => {
     const dispatch = useDispatch();
@@ -45,11 +43,6 @@ const Index = (props) => {
                     </div>
                     <CardRecentOrders />
                 </div>
-
-                {/* <div className="ps-section__right">
-                    <CardStatics />
-                    <CardTopCountries />
-                </div> */}
             </section>
         </ContainerDashboard>
 
